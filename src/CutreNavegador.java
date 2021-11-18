3dbcdb49e1933cb33aef91e50c4644733856df91
czujko <n.dynmate@protonmail.com>
2021-11-18 09:34:57 +0100

    Initial commit

---
+++ src/CutreNavegador.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CutreNavegador {
+
+    public static void main(String[] args){
+        String acumulador = "";
+        try {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Introduce la URL");
+            String direccion=sc.next().toLowerCase(Locale.ROOT);
+            direccion = (direccion.substring(0, 8).equals("https://")) ? direccion : "https://" + direccion;
+
+            URL miurl=new URL(direccion);
+            InputStream inputStream=miurl.openStream();
+            BufferedReader br=new BufferedReader(new InputStreamReader(inputStream));
+            String linea;
+            while((linea=br.readLine()) != null){
+                System.out.println(linea);
+                acumulador= acumulador + linea;
+            }
+            br.close();
+            inputStream.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int posicion = acumulador.indexOf("<title>");
+        int posicion2 = acumulador.indexOf("</title>");
+        String titulo = acumulador.substring(posicion + 7, posicion2);
+        System.out.println("Título: " + titulo);
+
+
+    }
+}
---
+++ src/Prueba.java
@@ -0,0 +1,23 @@
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Prueba {
+
+    public static void main(String[] args){
+        InetAddress inet;
+        String[] direcciones={"www.google.es", "www.xataka.com", "www.calasparra.org"};
+        try {
+            for (int i = 0; i < args.length; i++){
+                inet = InetAddress.getByName(args[i]);
+                System.out.println(inet.getCanonicalHostName());
+                System.out.println(inet.getHostAddress());
+            }
+            //System.out.println(inet.getCanonicalHostName());
+            //System.out.println(inet.getHostAddress());
+        }catch(UnknownHostException e){
+            e.printStackTrace();
+        }
+    }
+}
